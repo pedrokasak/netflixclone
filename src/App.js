aca562b8d3fe365bb6da53baf3f55dcd04d848df
Pedro Henrique <pedrohesm@gmail.com>
2021-09-02 12:32:01 -0300

    inserindo os cards e estilizando no css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import Tmdb from './Tmdb';
+import './App.css';
+import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
+
 
 export default () => {
 
+  const[movieList, setMovieList] = useState([]);
+  const[featuredData, setFeaturedData] = useState(null);
+
   useEffect(()=> {
     const loadAll = async ()  => {
+      //Consumindo a lista total de filmes
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
+
+      //Usando a Featured
+      let original = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (original[0].items.results.length - 1));
+      let chosen = original[0].items.results[randomChosen];
+      console.log(chosen)
     }
 
       loadAll();
   }, []);
 
   return (
-      <div>
-        Olá Mundo
+      <div className="page">
+        {featuredData &&
+          <FeatureMovie item={featuredData}/>
+        }   
+        <section className="lists">
+          {movieList.map((item, key)=> (
+            <MovieRow key={key} title={item.title} items={item.items} />
+          ))}
+        </section>
       </div>
 
   );
